7ed1d096a61a0d896d730c15902fc99068457b20
lelegod <2021m622@kuas.ac.jp>
2025-03-17 13:15:42 +0800

    feat: create startup card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,21 @@
 import SearchForm from '@/components/SearchForm';
+import StartupCard from '@/components/StartupCard';
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }>}) {
   const query = (await searchParams).query;
-
+  const posts = [
+    {
+    _createdAt: new Date(),
+    views: 55,
+    author: { _id: 1, name: 'Adrian' },
+    _id: 1,
+    description: 'This is a description',
+    image: 'https://cdn.pixabay.com/photo/2016/07/07/16/46/dice-1502706_640.jpg',
+    category: 'Robots',
+    title: 'We Robots',
+    },
+];
+  
   return (
     <>
       <section className="pink_container pattern">
@@ -11,6 +24,22 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
         </p>
         <SearchForm query={query}/>
+      </section>
+
+      <section className='section_container'>
+        <p className='text-30-semibold'>
+          {query ? `Search Result for"${query}"` : "All Startups"}
+        </p>
+
+        <ul className='mt-7 card_grid'>
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType) => (
+              <StartupCard key={post?._id} post={post}/>
+            ))
+          ) : (
+            <p>No Startup</p>
+          )}
+        </ul>
       </section>
     </>
   );
