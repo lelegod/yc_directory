b2834dc3567c490fc91e05c24cabfefe775f359b
lelegod <2021m622@kuas.ac.jp>
2025-03-17 19:19:59 +0800

    feat: add sanity live content API

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,11 @@
 import SearchForm from '@/components/SearchForm';
 import StartupCard, { StartupTypeCard } from '@/components/StartupCard';
-import { client } from '@/sanity/lib/client';
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { STARTUPS_QUERY } from '@/sanity/lib/queries';
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }>}) {
   const query = (await searchParams).query;
-  const posts = await client.fetch(STARTUPS_QUERY);
+  const { data: posts } = await sanityFetch({query: STARTUPS_QUERY});
   console.log(JSON.stringify(posts, null, 2));
   
   return (
@@ -33,6 +33,7 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
           )}
         </ul>
       </section>
+      <SanityLive/>
     </>
   );
 }
