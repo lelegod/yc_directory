193c7b333835d0f18c44e512de62f74d82efc39a
lelegod <2021m622@kuas.ac.jp>
2025-04-13 14:50:46 +0900

    feat: install sentry

--- lib/imageValidation.ts
+++ lib/imageValidation.ts
@@ -1,6 +1,6 @@
 "use server"
 
-export default async function imageValidation(url: string) {
+export default async function imagevalidation(url: string) {
     const res = await fetch(url, {method: "HEAD"});
     const contentType = res.headers.get("content-type");
     return contentType?.startsWith("image/");
--- lib/validation.ts
+++ lib/validation.ts
@@ -1,12 +1,12 @@
 import { z } from 'zod';
-import imageValidation from './imageValidation';
+import imagevalidation from './imagevalidation';
 
 export const formSchema = z.object({
     title: z.string().min(3).max(100),
     description: z.string().min(20).max(500),
     category: z.string().min(3).max(20),
     image: z.string().url().refine(async (url) => {
-        const isImage = await imageValidation(url);
+        const isImage = await imagevalidation(url);
         return isImage;
     }),
     pitch: z.string().min(10),
